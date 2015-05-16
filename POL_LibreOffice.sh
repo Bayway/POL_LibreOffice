ee65b0c7cdaf4909417309390da6698d95ca7006
Bayway <massimiliano.fiori@me.com>
2015-05-16 19:54:57 +0200

    Fix
    Now it works, but there are some crash during opening blank document

--- POL_LibreOffice.sh
+++ POL_LibreOffice.sh
@@ -8,45 +8,47 @@
 [ "$PLAYONLINUX" = "" ] && exit 0
 source "$PLAYONLINUX/lib/sources"
 
-TITLE="LibreOffice 4.4.2"
-PREFIX="LibreOffice_4.4.2"
+TITLE="LibreOffice 4.4.3"
+PREFIX="LibreOffice_4.4.3"
 
 
 POL_SetupWindow_Init
 POL_Debug_Init
 
-POL_System_SetArch "auto"
+POL_System_SetArch "x86"
 
 POL_SetupWindow_presentation "$TITLE" "LibreOffice" "http://www.libreoffice.org" "Massimiliano Fiori (massimiliano.fiori@aol.it)" "$PREFIX"
 
 POL_Wine_SelectPrefix "$PREFIX"
 POL_Wine_PrefixCreate
 
-POL_System_TmpCreate "LO"
-
+POL_System_TmpCreate "$PREFIX"
+POL_Call POL_Install_msvc100
 POL_SetupWindow_InstallMethod "LOCAL,DOWNLOAD"
 
 if [ "$INSTALL_METHOD" = "LOCAL" ]
 then
 POL_SetupWindow_browse "Please select the setup file to run." "$TITLE"
 POL_Wine_WaitBefore "$TITLE"
-#POL_SetupWindow_wait "$(eval_gettext 'Installation in progress.')" "$TITLE"
+
 POL_Wine msiexec /i "$APP_ANSWER"
 elif [ "$INSTALL_METHOD" = "DOWNLOAD" ]
 then
 cd "$POL_System_TmpDir"
-POL_Download "http://donate.libreoffice.org/home/dl/win-x86/4.4.2/en/LibreOffice_4.4.2_Win_x86.msi"
+POL_Download "http://download.documentfoundation.org/libreoffice/stable/4.4.3/win/x86/LibreOffice_4.4.3_Win_x86.msi"
 POL_Wine_WaitBefore "$TITLE"
 POL_SetupWindow_wait "$(eval_gettext 'Installation in progress.')" "$TITLE"
-POL_Wine msiexec /i "POL_System_TmpDelete/LibreOffice_4.4.2_Win_x86.msi"
+POL_Wine msiexec /i "$POL_System_TmpDir/LibreOffice_4.4.3_Win_x86.msi"
 fi
+
+
 
 POL_System_TmpDelete
 
 #POL_SetupWindow_VMS "128"
 POL_Wine_reboot
 
-POL_Shortcut "libreoffice.exe" "$TITLE"
+POL_Shortcut "soffice.exe" "$TITLE"
 
 POL_SetupWindow_Close
 exit
